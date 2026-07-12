6907f93ec8a1e4a6de6b5ba4fe1daff14f05e951
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2026-07-12 13:15:25 -0400

    refactor: remove unused integration-related use cases and ports

--- src/context/integrations/domain/ports/out/index.ts
+++
@@ -1,6 +0,0 @@
-// Output Ports (Driven) - Barrel Export
-export * from './integration-provider.port';
-export * from './user-provisioning.port';
-export * from './linked-account-repository.port';
-export * from './token-encryption.port';
-export * from './oauth-state-repository.port';
